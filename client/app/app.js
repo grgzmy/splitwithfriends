10493479339a770b9e036c66af073e061b9acc1a
Rami Sayar <rami.sayar@gmail.com>
2015-04-19 13:21:58 -0400

    Added yo angularfullstack generated code.

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('splitwithfriendsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
