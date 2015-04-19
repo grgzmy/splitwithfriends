e84a62f14537afb5b293ffa716dda992504c655d
Rami Sayar <rami.sayar@gmail.com>
2015-04-19 13:40:45 -0400

    added expense and friend endpoints using yo scaffolding.

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,8 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/expenses', require('./api/expense'));
+  app.use('/api/friends', require('./api/friend'));
   app.use('/api/things', require('./api/thing'));
   
   // All undefined asset or api routes should return a 404
